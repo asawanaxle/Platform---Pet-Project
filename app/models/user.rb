ed219ed3fe5acd9438b620c1575909f689fb73cb
asawanaxle <Aditi.Sawant@data-axle.com>
2022-09-08 15:42:06 +0530

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+
+  has_many  :reservations
+
+  enum role: [:user, :editor, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+ 
+  
+  
+end
